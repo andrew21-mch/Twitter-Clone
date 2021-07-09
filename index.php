9a550f83df291d57ff0d1648b18877a04f7a80d4
andrew21-mch <nfonandrew73@gmail.com>
2021-07-09 09:09:18 +0100

    index page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter clone</title>
+
+    <link rel="stylesheet" href="frontend/assest/css/fontawesome/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="frontend/assest/css/style.css">
+</head>
+<body>
+    <section class="main-page">
+        <div class="left">
+            <div class="left-content">
+                <div>
+                    <i class = "fas fa-search"></i>
+                    <h3 class="left-content-heading">Find your interest</h3>
+                    
+                </div>
+                <div>
+                    <i class = "fas fa-users"></i>
+                    <h3 class="left-content-heading">Explore what people are talking about</h3>
+                    
+                </div>
+                <div>
+                    <i class = "fas fa-comment"></i>
+                    <h3 class="left-content-heading">Join your interest</h3>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="right">Right</div>
+
+    <footer>footer</footer>
+    
+    </section>
+
+</body>
+</html>
