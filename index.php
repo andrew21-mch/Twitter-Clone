549ebaa8894863dc8e416762831be8ba7285848f
andrew21-mch <nfonandrew73@gmail.com>
2021-07-10 18:11:54 +0100

    updatied the index page

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twitter clone</title>
-
-    <link rel="stylesheet" href="frontend/assest/css/fontawesome/css/all.min.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="frontend/assets/css/style.css">
-</head>
+<?php 
+include "backend/shared/head.inc.php";
+?>
 <body>
     <section class="main-page">
         <div class="left">
