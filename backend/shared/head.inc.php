7eab6f82079275df2ae3ce8e059b5d8744be2049
andrew21-mch <nfonandrew73@gmail.com>
2021-07-10 19:35:52 +0100

    restrucring link

--- backend/shared/head.inc.php
+++ backend/shared/head.inc.php
@@ -11,10 +11,10 @@ if(!isset($pagetitle)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pagetitle ?></title>
 
-    <link rel="stylesheet" href="frontend/asset/css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="frontend/assets/fontawesome/css/all.min.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-    <link rel="styleshett" href="frontend/asset/css/css/style.css">
+    <link rel="stylesheet" href="frontend/assets/css/master.css">
 </head>
